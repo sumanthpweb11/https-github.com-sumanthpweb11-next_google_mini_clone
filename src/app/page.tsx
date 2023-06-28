4515d3f4bdd734c5e3a3fa87d64b16214a6830d2
sumanthpweb11 <110541334+sumanthpweb11@users.noreply.github.com>
2023-06-28 11:16:50 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Main from "@/components/Main";
+
+export default function Home() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
